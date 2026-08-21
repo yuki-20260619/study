509cc5d2d6258538228a3c591270e36d838002e7
yuki-20260619 <blauervogel.yuki@gmail.com>
2026-08-22 06:25:09 +0900

    Java学習構成をStudyとPracticeに整理

---
+++ src/study/java/practice/standardInput/day02_20260822/Main.java
@@ -0,0 +1,12 @@
+package study.java.practice.standardInput.day02_20260822;
+
+public class Main {
+    public static void main(String[] args) {
+        // ----------------------------------------
+        // [出力とコメント - 文字列の出力2]
+        // ----------------------------------------
+        // 文字列"sky"を出力してください
+
+        System.out.println("sky");
+    }
+}
